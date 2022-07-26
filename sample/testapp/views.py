53f94458b712923a73b904564b2d384f452cc221
Ankit FSD <ankitpy.snable@gmail.com>
2022-07-26 14:59:59 +0530

    new views
    create new views raks for राकेश चे भांडे.

--- sample/testapp/views.py
+++ sample/testapp/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def greeting(request):
     return HttpResponse("<h1>Hello Python...</h1>")
+
+def raks(request):
+    return HttpResponse("<h1>राकेश चे भांडे. </h2>")
