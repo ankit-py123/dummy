38f0d1c830fb937d3a84dbb6db2fc063aeff9970
Ankit FSD <ankitpy.snable@gmail.com>
2022-07-26 13:14:44 +0530

    views change
    greeting views change

--- sample/testapp/views.py
+++ sample/testapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def greeting(request):
-    return HttpResponse("<h1>Hello World...</h1>")
+    return HttpResponse("<h1>Hello Python...</h1>")
